39757c6a2054514a1cf6df8294234b625d2a4b65
Damini9112 <deshmukhdamu25@gmail.com>
2022-05-25 12:01:22 +0530

    hello

---
+++ Demo.java
@@ -0,0 +1,12 @@
+package gitush;
+
+public class Demo {
+public static void main(String[] args) {
+	int[] A= {1,2,3,4};
+	for(int i=0;i<A.length;i++) {
+		if(A[i]%2==0) {
+			System.out.println(" the given num is even"+A[i]);
+		}
+			}
+}
+}
